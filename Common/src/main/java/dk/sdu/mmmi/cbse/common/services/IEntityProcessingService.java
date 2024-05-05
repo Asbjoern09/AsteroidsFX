364ddd763e0d829cb79fa4a738820a1be979d362
Asbjørn <asbjorn454@gmail.com>
2024-05-05 17:53:48 +0200

    Fix module-info, add javadoc to interfaces

--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IEntityProcessingService.java
@@ -3,16 +3,21 @@ package dk.sdu.mmmi.cbse.common.services;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
+/**
+ * This interface defines the contract for processing entities in the game world.
+ */
 public interface IEntityProcessingService {
 
     /**
+     * Processes entities based on the provided game data and world state.
      *
+     * Preconditions: gameData must not be null, world must not be null, dt must be greater than or equal to 0
+     * Postconditions: the entities have been updated, position, hp, etc.
      *
+     * @param gameData gameData contains and modifies information about the game config, displayheight, keyinputs etc.
+     * @param world    world contains entities and their properties, (player, enemies, etc.)
+     * @param dt       the time since the last update in seconds (delta time), necessary for the game to run properly on linux.
      *
-     * @param gameData
-     * @param world
-     * @throws
      */
-
     void process(GameData gameData, World world, double dt);
 }
--- Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
+++ Common/src/main/java/dk/sdu/mmmi/cbse/common/services/IPostEntityProcessingService.java
@@ -4,10 +4,19 @@ import dk.sdu.mmmi.cbse.common.data.GameData;
 import dk.sdu.mmmi.cbse.common.data.World;
 
 /**
- *
- * @author jcs
+ * This interface defines the contract for processing entities after the main entity processing, such as collision based on new position.
  */
 public interface IPostEntityProcessingService {
 
+    /**
+     * Processes entities after the main entity processing step based on the provided game data and world state, such as collision based on new position.
+     *
+     * Preconditions: gameData must not be null, world must not be null, dt must be greater than or equal to 0
+     * Postconditions: the entities have been handled in their updated state, e.g. collisioncheck
+     *
+     * @param gameData gameData contains and modifies information about the game config, displayheight, keyinputs etc.
+     * @param world    world contains entities and their properties, (player, enemies, etc.)
+     * @param dt       the time since the last update in seconds (delta time), necessary for the game to run properly on linux.
+     */
     void process(GameData gameData, World world, double dt);
 }
