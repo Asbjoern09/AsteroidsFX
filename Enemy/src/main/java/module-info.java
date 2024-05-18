3e4825b5ab5a6d00dbc1f0420b53c6f68e6e8c32
Asbjørn <asbjorn454@gmail.com>
2024-05-18 12:07:35 +0200

    update module-info

--- Enemy/src/main/java/module-info.java
+++ Enemy/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 
 import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
 import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
 module Enemy {
     requires Common;
@@ -9,5 +10,5 @@ module Enemy {
     uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
     provides IGamePluginService with dk.sdu.mmmi.cbse.enemysystem.EnemyPlugin;
     provides IEntityProcessingService with dk.sdu.mmmi.cbse.enemysystem.EnemyControlSystem;
-    
+    provides IPostEntityProcessingService with dk.sdu.mmmi.cbse.SplitPackageTest;
 }
