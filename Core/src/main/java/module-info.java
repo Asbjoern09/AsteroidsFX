1d8a1ec4c4056489f0a132b5dd4a62261e9516b1
Asbjørn <asbjorn454@gmail.com>
2024-05-20 20:05:54 +0200

    update module-info

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -5,7 +5,6 @@ module Core {
     uses dk.sdu.mmmi.cbse.common.services.IGamePluginService;
     uses dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
     uses dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
-
 }
 
 
--- Player/src/main/java/module-info.java
+++ Player/src/main/java/module-info.java
@@ -5,7 +5,6 @@ import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
 module Player {
     requires Common;
     requires CommonBullet;
-    requires CommonAsteroid;
     uses dk.sdu.mmmi.cbse.common.bullet.BulletSPI;
     provides IGamePluginService with dk.sdu.mmmi.cbse.playersystem.PlayerPlugin;
     provides IEntityProcessingService with dk.sdu.mmmi.cbse.playersystem.PlayerControlSystem;
